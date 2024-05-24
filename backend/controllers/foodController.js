661e53edb7cce6e8a3e28fce432afac7ecbfcaf6
Usha_Gotame <ushagotame000@gmail.com>
2024-05-24 22:14:57 +0545

    curd

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -29,6 +29,7 @@
 
 import foodModel from "../models/foodModel.js";
 import fs from "fs";
+import foodRouter from "../routes/foodRoute.js";
 
 // Add food item
 const addFood = async (req, res) => {
@@ -51,4 +52,30 @@ const addFood = async (req, res) => {
   }
 };
 
-export { addFood };
+//list all food list
+
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "error" });
+  }
+};
+
+// remove item list
+
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => {});
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ sucess: true, message: "Food Deleted" });
+  } catch (error) {
+    console.log(error);
+    res.json({ sucess: false, message: "error" });
+  }
+};
+
+export { addFood, listFood, removeFood };
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { addFood } from "../controllers/foodController.js";
+import {
+  addFood,
+  listFood,
+  removeFood,
+} from "../controllers/foodController.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
@@ -8,11 +12,16 @@ const foodRouter = express.Router();
 const storage = multer.diskStorage({
   destination: "uploads",
   filename: (req, file, cb) => {
-    return cb(null, "${Data.now()}${file.originalname}");
+    return cb(null, "${Data.now()}-${file.originalname}");
   },
 });
+
+//initialize upload variable
 const upload = multer({ storage: storage });
 
+//route multer
 foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
